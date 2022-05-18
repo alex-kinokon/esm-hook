cad611348c07b2b06528b01c2dc7171279b37ec9
Alex <8125011+proteriax@users.noreply.github.com>
2022-05-17 21:22:15 -0400

    Add test

---
+++ test/require.test.ts
@@ -0,0 +1,28 @@
+import assert from "assert/strict";
+import { describe, it } from "mocha";
+
+describe("require", () => {
+  it("should fail to require node-fetch", () => {
+    assert.throws(
+      () => require("node-fetch"),
+      { code: "ERR_REQUIRE_ESM" },
+      'require("node-fetch") should fail'
+    );
+  });
+
+  it("should require node-fetch", () => {
+    const unregister = require("../register");
+    assert.doesNotThrow(() => {
+      const fetch = require("node-fetch");
+      assert.equal(typeof fetch, "object");
+      assert.equal(typeof fetch.default, "function");
+    });
+    unregister();
+    delete require.cache[require.resolve("node-fetch")];
+    assert.throws(
+      () => require("node-fetch"),
+      { code: "ERR_REQUIRE_ESM" },
+      "unregister hook failed"
+    );
+  });
+});
